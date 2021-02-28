fa6a64e526a3e7ada2908295c64316486e67bed0
hurek <rudrobinrood@gmail.com>
2021-03-01 01:59:49 +0300

    Fix: typo in credentials

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_creds():
     return ServiceAccountCredentials.from_json_keyfile_name(
         "src/creds.json",
         [
-            "https://spreadsheets.google.com/feedsname",
+            "https://spreadsheets.google.com/feeds",
             "https://www.googleapis.com/auth/drive",
             "https://www.googleapis.com/auth/spreadsheets",
         ],
